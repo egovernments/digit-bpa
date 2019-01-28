30375e4010faa311d3895bbc914ae4b9cee2d929
venki <venki@egovernments.org>
2019-01-28 17:15:12 +0530

    [PHOENIX-8514] - Corrected the scripts for deployment.

--- egov/egov-collection/src/main/resources/db/migration/main/V20181022172002__collection_hdfc_payment.sql
+++ egov/egov-collection/src/main/resources/db/migration/main/V20181022172002__collection_hdfc_payment.sql
@@ -7,7 +7,3 @@ scheme,subscheme,servicecategory,isvoucherapproved,vouchercutoffdate,created_by,
 'https://kozhikode-uat-phase2.egovernments.org/collection/citizen/onlineReceipt-acceptMessageFromPaymentGateway.action', 'P', 'HDFC',
 (select id from fund where code='01'), null, null, false, null, null, (select id from egcl_servicecategory where code='HDFC'), false, now(), 1, now(),
  1, now(),null);
-
-Insert into EGCL_SERVICE_INSTRUMENTACCOUNTS(id, instrumenttype, servicedetails, chartofaccounts, createdby, createddate, lastmodifiedby, lastmodifieddate)
-values(nextval('seq_egcl_service_instrumentaccounts'),(select id from egf_instrumenttype  where type ='online'),(select id from egcl_servicedetails where name='HDFC Bank Payment Gateway' and code='HDFC'),
-(select id from chartofaccounts where glcode='4318001'),1,now(),1,now());
