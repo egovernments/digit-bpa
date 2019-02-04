5e534dbb017d33caac0ec92867e016b67901b9cd
MahammadNizam <mahammad.nizam@egovernments.org>
2019-02-04 18:35:02 +0530

    [PHOENIX-8492] : Updated version column values in the tables.

---
+++ egov/egov-bpa/src/main/resources/db/migration/main/V20190204180146__update_version_value.sql
@@ -0,0 +1,23 @@
+update eg_drawingofficer set version =0;
+
+update eg_appconfig  set version=0;
+
+update eg_appconfig_values  set version=0;
+
+update eg_messagetemplate set version =0;
+
+update eg_wf_states set version =0; 
+
+update egpt_mutation_fee_details set version =0;
+
+update egpgr_complaintstatus_mapping set version=0;
+
+update egpt_mutation_fee_details set version =0;
+
+update egpt_application_type set version=0;
+
+update egpt_department set version=0;
+
+update egpt_document_type set version=0;
+
+update eg_role set version=0;
