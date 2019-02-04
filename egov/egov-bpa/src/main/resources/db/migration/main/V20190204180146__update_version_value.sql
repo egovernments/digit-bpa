62c78ee54c0db32291a2c7eeb578c47d5c8e1ca6
MahammadNizam <mahammad.nizam@egovernments.org>
2019-02-04 18:41:04 +0530

    [PHOENIX-8492] : removed egpt tables.

--- egov/egov-bpa/src/main/resources/db/migration/main/V20190204180146__update_version_value.sql
+++ egov/egov-bpa/src/main/resources/db/migration/main/V20190204180146__update_version_value.sql
@@ -8,16 +8,6 @@ update eg_messagetemplate set version =0;
 
 update eg_wf_states set version =0; 
 
-update egpt_mutation_fee_details set version =0;
-
 update egpgr_complaintstatus_mapping set version=0;
-
-update egpt_mutation_fee_details set version =0;
-
-update egpt_application_type set version=0;
-
-update egpt_department set version=0;
-
-update egpt_document_type set version=0;
 
 update eg_role set version=0;
