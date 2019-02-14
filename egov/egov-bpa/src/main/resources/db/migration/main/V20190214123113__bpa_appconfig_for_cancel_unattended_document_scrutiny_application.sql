791214622725a86a86bf8ca9dc287adc1330d1ca
rani-priyanka <priyanka.rani@egovernments.org>
2019-02-14 12:37:39 +0530

    [PHOENIX-8576]:Auto cancel application if not attended for document
    scrutiny can be configurable.

---
+++ egov/egov-bpa/src/main/resources/db/migration/main/V20190214123113__bpa_appconfig_for_cancel_unattended_document_scrutiny_application.sql
@@ -0,0 +1,3 @@
+INSERT INTO eg_appconfig ( ID, KEY_NAME, DESCRIPTION, VERSION, MODULE ) VALUES (nextval('SEQ_EG_APPCONFIG'), 'IS_AUTO_CANCEL_UNATTENDED_DOCUMENT_SCRUTINY_APPLICATION', 'Is auto cancel unattended documnet scrutiny application',0, (select id from eg_module where name='BPA')); 
+
+INSERT INTO eg_appconfig_values ( ID, KEY_ID, EFFECTIVE_FROM, VALUE, VERSION ) VALUES (nextval('SEQ_EG_APPCONFIG_VALUES'), (SELECT id FROM EG_APPCONFIG WHERE KEY_NAME='IS_AUTO_CANCEL_UNATTENDED_DOCUMENT_SCRUTINY_APPLICATION' and module= (select id from eg_module where name='BPA')), current_date,'YES',0);
