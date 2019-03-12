a5918ec43ec11ea946e48a441e20959cb37bb0d9
kiranmayi <kiranmayi.pulluru@tarento.com>
2019-03-12 15:06:46 +0530

    [PHOENIX-8760] - corrected syntax error

--- egov/egov-commons/src/main/resources/db/migration/main/V20190312142830__common_update_checklists_description.sql
+++ egov/egov-commons/src/main/resources/db/migration/main/V20190312142830__common_update_checklists_description.sql
@@ -12,16 +12,16 @@ update eg_checklist set description ='Whether Front yard, Side yard 1, Side yard
 and conditions with regards to overhangs stipulated in rules 24-10, 24-11, 62-2 are complied with?' where code='OCPLANSCRUTINYDRAWING-06';
 
 update eg_checklist set description ='Whether the various uses/ occupancies of different spaces in the building/s as specified in the generated EDCR report for the particular application, are in accordance with 
-the uses specified against different spaces specified in the uploaded  drawings in pdf format, based on occupancy classes defined as per rules and related amendments?' where code='OCPLANSCRUTINYDRAWING-03'
+the uses specified against different spaces specified in the uploaded  drawings in pdf format, based on occupancy classes defined as per rules and related amendments?' where code='OCPLANSCRUTINYDRAWING-03';
 
 update eg_checklist set description ='Whether the practicality of the provided car parking, two wheeler parking, loading and unloading space, manoeuvring space, slope, maximum open yard area allowable to 
-be covered by parking, width of driveway etc. are ensured with regards to provisions as per rule and related amendments?' where code='OCPLANSCRUTINYRULE-05'
+be covered by parking, width of driveway etc. are ensured with regards to provisions as per rule and related amendments?' where code='OCPLANSCRUTINYRULE-05';
 
 update eg_checklist set description ='Whether the practicality of the provided car parking, two wheeler parking, loading and unloading space, manoeuvring space, slope, maximum open yard area allowable to 
-be covered by parking, width of driveway etc. are ensured with regards to provisions as per rule and related amendments?' where code='PLANSCRUTINY-04'
+be covered by parking, width of driveway etc. are ensured with regards to provisions as per rule and related amendments?' where code='PLANSCRUTINY-04';
 
 update eg_checklist set description ='Whether the various uses/ occupancies of different spaces in the building/s as specified in the generated EDCR report for the particular application, are in accordance with 
-the uses specified against different spaces specified in the uploaded  drawings in pdf format, based on occupancy classes defined as per rules and related amendments?' where code='PLANSCRUTINYDRAWING-03'
+the uses specified against different spaces specified in the uploaded  drawings in pdf format, based on occupancy classes defined as per rules and related amendments?' where code='PLANSCRUTINYDRAWING-03';
 
 update eg_checklist set description ='Whether Front yard, Side yard 1, Side yard 2, Rear yard and plot boundary polygons are accurately marked (Plot boundaries shall be excluding the road widening area if any), 
-and conditions with regards to overhangs stipulated in rules 24-10, 24-11, 62-2 are complied with?' where code='PLANSCRUTINYDRAWING-05'
+and conditions with regards to overhangs stipulated in rules 24-10, 24-11, 62-2 are complied with?' where code='PLANSCRUTINYDRAWING-05';
