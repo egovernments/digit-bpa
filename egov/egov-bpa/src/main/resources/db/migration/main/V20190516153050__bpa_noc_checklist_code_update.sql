c3b1acd2595768f84368fc7a9f5f678f982e0134
rani-priyanka <priyanka.rani@egovernments.org>
2019-05-16 15:34:43 +0530

    [BPA-152]:NOC Configurations-code update script

---
+++ egov/egov-bpa/src/main/resources/db/migration/main/V20190516153050__bpa_noc_checklist_code_update.sql
@@ -0,0 +1,5 @@
+update eg_checklist set code='AAI_NOC' where code='NOC-01';
+update eg_checklist set code='NMA_NOC' where code='NOC-02';
+update eg_checklist set code='IDA_NOC' where code='NOC-03';
+update eg_checklist set code='FIRE_NOC' where code='NOC-04';
+update eg_checklist set code='MOEF_NOC' where code='NOC-05';
