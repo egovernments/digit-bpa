ec96bf91b707445acb6d02e2ba4328d4cba12ce8
priyajais <priya.jaiswal@egovernments.org>
2019-11-14 14:44:59 +0530

    [DBS-7,DBS-8] Showing twice Occupancy Certificate Plan Scrutiny in Portal Page

---
+++ egov/egov-portal/src/main/resources/db/migration/main/V20191114151045__portal_plan_scrutiny_service.sql
@@ -0,0 +1,6 @@
+delete from egp_portalservice where code = 'New Occupancy Certificate Plan Scrutiny';
+delete from egp_portalservice where code = 'Resubmit Occupancy Certificate Plan Scrutiny';
+
+Insert into EGP_PORTALSERVICE (id,module,code,sla,version,url,isactive,name,userservice,businessuserservice,helpdoclink,createdby,createddate,lastmodifieddate,lastmodifiedby) values(nextval('seq_egp_portalservice'),(select id from eg_module where name='Digit DCR'),'New Occupancy Certificate Plan Scrutiny',null,0,'/edcr/occupancy-certificate/plan/submit','true','New Occupancy Certificate Plan Scrutiny','true','true','/edcr/occupancy-certificate/plan/submit',1,now(),now(),1);
+
+Insert into EGP_PORTALSERVICE (id,module,code,sla,version,url,isactive,name,userservice,businessuserservice,helpdoclink,createdby,createddate,lastmodifieddate,lastmodifiedby) values(nextval('seq_egp_portalservice'),(select id from eg_module where name='Digit DCR'),'Resubmit Occupancy Certificate Plan Scrutiny',null,0,'/edcr/occupancy-certificate/plan/resubmit','true','Resubmit Occupancy Certificate Plan Scrutiny','true','true','/edcr/occupancy-certificate/plan/resubmit',1,now(),now(),1);
